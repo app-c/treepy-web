4159573dc4e51597a41f0f8e180e463c35c8035e
app-c <william@app-com.digital>
2023-05-25 23:02:54 -0300

    change hostname

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const local = 'http://localhost:3333'
-const server = 'https://treepy.app-com.digital'
+const server = 'https://treepy-server.appcom.dev'
 const seg = `${server}/pay-pag`
 
 export const pag = axios.create({
